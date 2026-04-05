05c98a235513ff07dd40390fbeb31e9e57086196
Playful Mathematician <me@playfulmathematician.com>
2026-04-05 10:09:13 +0800

    great, great, great, great, great
    great, great, great, great, great

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import mdx from "@mdx-js/rollup";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import rehypeHighlight from "rehype-highlight";
-
+import remarkMath from "remark-math";
+import rehypeMathjax from "rehype-mathjax";
 export default defineConfig({
   plugins: [
     react(),
@@ -12,8 +13,9 @@ export default defineConfig({
       remarkPlugins: [
         remarkFrontmatter,
         [remarkMdxFrontmatter, { name: "frontmatter" }],
+        remarkMath,
       ],
-      rehypePlugins: [rehypeHighlight],
+      rehypePlugins: [rehypeHighlight, rehypeMathjax],
     }),
   ],
 });
